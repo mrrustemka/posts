00dff3ce850639e63861af75d7bb4ae6b6975a9f
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-12 20:07:40 +0300

    create-new-blog-post-using-fetch

--- src/posts/types.ts
+++ src/posts/types.ts
@@ -1,1 +1,2 @@
 export type PostData = { id: number; title: string; description: string };
+export type NewPostData = {  title: string;  description: string;};export type SavedPostData = {  id: number;};
