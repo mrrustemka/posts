74187fe74ec40066d30aa5a8e08f2639b6de9b19
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-12 20:39:42 +0300

    update-data-using-react-query

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,11 +1,11 @@
-import { NewPostData, PostData } from "./types";
+import { PostData } from "./types";
 import { PostsList } from "./PostsList";
 import { savePost } from "./savePost";
 import { NewPostForm } from "./NewPostForm";
 import { assertIsPosts, getPosts } from "./getPosts";
 import { Suspense } from "react";
 import { useLoaderData, Await } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 type Data = { posts: PostData[] };
 export function assertIsData(data: unknown): asserts data is Data {
@@ -26,19 +26,28 @@ export function PostsPage() {
     isFetching,
     data: posts,
   } = useQuery(["postsData"], getPosts);
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(savePost, {
+    onSuccess: (savedPost) => {
+      queryClient.setQueryData<PostData[]>(["postsData"], (oldPosts) => {
+        if (oldPosts === undefined) {
+          return [savedPost];
+        } else {
+          return [savedPost, ...oldPosts];
+        }
+      });
+    },
+  });
   // const data = useLoaderData();
   // assertIsData(data);
 
-  async function handleSave(newPostData: NewPostData) {
-    await savePost(newPostData);
-  }
   if (isLoading || posts === undefined) {
     return <div className="w-96 mx-auto mt-6">Loading ... </div>;
   }
   return (
     <div className="w-96 mx-auto mt-6">
       <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
-      <NewPostForm onSave={handleSave} />
+      <NewPostForm onSave={mutate} />
       <PostsList posts={posts} />
       {/* <Suspense fallback={<div>Fetching...</div>}>
         <Await resolve={data.posts}>
