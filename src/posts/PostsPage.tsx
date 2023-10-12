687207dc0a50e8b09ba6a24ed8e0d72928e8ad53
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-12 20:15:07 +0300

    create-blog-post-form-component

--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { getPosts } from "./getPosts";
-import { PostData } from "./types";
+import { PostData, NewPostData } from "./types";
 import { PostsList } from "./PostsList";
+import { savePost } from "./savePost";
+import { NewPostForm } from "./NewPostForm";
 
 export function PostsPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,12 +21,17 @@ export function PostsPage() {
       cancel = true;
     };
   }, []);
+  async function handleSave(newPostData: NewPostData) {
+    const newPost = await savePost(newPostData);
+    setPosts([newPost, ...posts]);
+  }
   if (isLoading) {
     return <div className="w-96 mx-auto mt-6"> Loading ... </div>;
   }
   return (
     <div className="w-96 mx-auto mt-6">
       <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
+      <NewPostForm onSave={handleSave} />
       <PostsList posts={posts} />
     </div>
   );
